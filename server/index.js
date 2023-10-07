00d45cd0a6d2283ea57f74dc145ca26e4517f711
Kenny Tan <kjtan3@dons.usfca.edu>
2023-10-07 16:31:21 -0700

    middlware cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 5000
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`server has started on port ${port}`)
